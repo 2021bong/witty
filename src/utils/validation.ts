77510bcc9ddf5a2f266a1914358a547ab912395c
Bong <2021bongwonny@gmail.com>
2022-11-18 01:10:57 +0900

    🩹 Modify : validation 함수들 지역 변수 이름 변경 - ~Format -> ~Regexp으로 직관적으로 변수 이름 변경 - 아이디 길이 유효성 검사 수정

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,22 +1,22 @@
 export const checkEmail = (email: string) => {
-  const emailFormat =
+  const emailRegexp =
     /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-  return email !== '' && email !== undefined && emailFormat.test(email);
+  return email !== '' && email !== undefined && emailRegexp.test(email);
 };
 
 export const checkName = (name: string) => {
-  const nameFormat =
-    /^[0-9a-zA-Z-ㄱ-힣\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\" ].{0,7}$/;
-  return name !== '' && name !== undefined && nameFormat.test(name);
+  const nameRegexp =
+    /^[0-9a-zA-Z-ㄱ-힣-\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\" ].{0,7}$/;
+  return name !== '' && name !== undefined && nameRegexp.test(name);
 };
 
 export const checkId = (id: string) => {
-  const idFormat = /^(?=.*[a-zA-z])(?=.*[0-9]).{6,12}$/;
-  return id !== '' && id !== undefined && idFormat.test(id);
+  const idRegexp = /^(?=.*[a-zA-z])(?=.*[0-9]).{6,15}$/;
+  return id !== '' && id !== undefined && idRegexp.test(id);
 };
 
 export const checkPw = (pw: string) => {
-  const pwFormat =
-    /^(?=.*?[a-zA-z])(?=.*?[0-9])(?=.*?[\@\#\*\!\^])(?!(?=.*[\|{\}\[\]\/?.,;:|\)~`\-_+<>\$%&\\\=\(\'\"]).).{8,16}$/;
-  return pw !== '' && pw !== undefined && pwFormat.test(pw);
+  const pwRegexp =
+    /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[@#*!^])(?!(?=.*[\|{\}\[\]\/?.,;:|\)~`\-_+<>\$%&\\\=\(\'\"]).).{8,16}$/;
+  return pw !== '' && pw !== undefined && pwRegexp.test(pw);
 };
