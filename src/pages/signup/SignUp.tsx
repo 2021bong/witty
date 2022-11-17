cb4e1ca20ce12300ba7327c09b874b5f67d85b5b
Bong <2021bongwonny@gmail.com>
2022-11-17 22:14:09 +0900

    ♻️Refactor : SignUp 컴포넌트 스타일드 컴포넌트 파일 분리

--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import styled from 'styled-components';
 import {
   checkEmail,
   checkName,
@@ -15,6 +14,7 @@ import {
   ValidationRes,
   SignUpRes,
 } from '../../utils/interface';
+import { Main } from './SignUp.styled';
 
 const SignUp = () => {
   const [idValue, setIdValue] = useState('');
@@ -213,142 +213,3 @@ const SignUp = () => {
 };
 
 export default SignUp;
-
-const Main = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 90%;
-  min-height: 600px;
-  max-width: 500px;
-  margin: 100px auto;
-  padding: 3rem;
-  border: 1px solid #ddd;
-  border-radius: 1rem;
-  color: ${({ theme }) => theme.text};
-
-  .title {
-    font-size: 2rem;
-    margin-bottom: 60px;
-    color: ${({ theme }) => theme.mainColor};
-  }
-
-  .formContainer {
-    width: 80%;
-    min-width: 270px;
-
-    .inputContainer {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      margin-bottom: 30px;
-
-      .desc {
-        font-weight: 400;
-        margin-top: 8px;
-        font-size: 0.9rem;
-        color: ${({ theme }) => theme.subColor};
-      }
-
-      .true {
-        color: ${({ theme }) => theme.mainColor2};
-      }
-
-      .inputBtnContainer {
-        display: flex;
-        width: 100%;
-        justify-content: center;
-        align-items: center;
-
-        .inputWithBtn {
-          margin-right: 5px;
-          margin-bottom: 0;
-        }
-
-        .btn {
-          height: 100%;
-          min-width: 80px;
-          padding: 8px;
-          border: none;
-          border-radius: 8px;
-          color: #fff;
-          background-color: ${({ theme }) => theme.mainColor};
-
-          &:hover {
-            background-color: ${({ theme }) => theme.subColor};
-          }
-
-          &:active {
-            background-color: ${({ theme }) => theme.mainColor2};
-          }
-        }
-      }
-
-      label {
-        margin-bottom: 5px;
-        font-weight: 700;
-      }
-
-      input {
-        display: inline-block;
-        width: 100%;
-        padding: 8px;
-        border-radius: 0.6rem;
-
-        &:first-child {
-          margin-bottom: 10px;
-        }
-      }
-    }
-  }
-
-  .btnContainer {
-    width: 80%;
-    min-width: 270px;
-
-    .btn {
-      width: 100%;
-      height: 40px;
-      border: none;
-      border-radius: 8px;
-      color: #fff;
-      background-color: ${({ theme }) => theme.mainColor};
-
-      &:hover {
-        background-color: ${({ theme }) => theme.subColor};
-      }
-
-      &:active {
-        background-color: ${({ theme }) => theme.mainColor2};
-      }
-
-      &:first-of-type {
-        margin-bottom: 5px;
-      }
-    }
-
-    .cancelBtn {
-      width: 100%;
-      height: 40px;
-      border: 1px solid #ccc;
-      border-radius: 8px;
-      color: ${({ theme }) => theme.text};
-      background-color: #fff;
-
-      &:hover {
-        color: #fff;
-        background-color: ${({ theme }) => theme.mainColor};
-      }
-
-      &:active {
-        color: #fff;
-        background-color: ${({ theme }) => theme.mainColor2};
-      }
-
-      &:first-of-type {
-        margin-bottom: 5px;
-      }
-    }
-  }
-`;
