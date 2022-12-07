a11c4da5239dcc926be27d76c979e3408607ba1b
Bong <2021bongwonny@gmail.com>
2022-12-07 17:15:21 +0900

    💄Add : write 페이지 UI 추가 - 선택 카테고리 UI 추가 - 사진 첨부 UI 추가

--- src/pages/main/write/Write.tsx
+++ src/pages/main/write/Write.tsx
@@ -1,13 +1,38 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, MouseEvent, ChangeEvent } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
+import { IoMdPhotos } from 'react-icons/io';
 import Dock from '../../../components/Dock';
 import Greeting from '../../../components/Greeting';
 
 const Write = () => {
   const [textValue, setTextValue] = useState('');
+  const [categorys, setCategorys] = useState([
+    { id: 1, name: '#오늘아무거나', selected: true },
+    { id: 2, name: '#오늘기분', selected: false },
+    { id: 3, name: '#오늘소비', selected: false },
+    { id: 4, name: '#오늘잡담', selected: false },
+    { id: 5, name: '#오늘먹은것', selected: false },
+  ]);
+  const [photos, setPhotos] = useState([
+    { id: 1, file: null },
+    { id: 2, file: null },
+    { id: 3, file: null },
+    { id: 4, file: null },
+  ]);
   const navigate = useNavigate();
+
+  const handleSelectedCategory = (e: MouseEvent<HTMLLIElement>) => {
+    const selectedName = e.currentTarget.textContent;
+    setCategorys((prev) =>
+      prev.map((category) =>
+        category.name === selectedName
+          ? { ...category, selected: true }
+          : { ...category, selected: false }
+      )
+    );
+  };
 
   const handleWriteText = (e: ChangeEvent<HTMLTextAreaElement>) => {
     if (e.target.value.length > 140) {
@@ -22,7 +47,12 @@ const Write = () => {
       axios
         .post(
           'http://localhost:8000/posts',
-          { content: textValue },
+          {
+            content: textValue,
+            category: categorys.filter(
+              (category) => category.selected === true
+            )[0].name,
+          },
           { headers: { Authorization: localStorage.getItem('token') } }
         )
         .then((res) => navigate('/'))
@@ -51,6 +81,27 @@ const Write = () => {
     <Container>
       <Greeting text='What are you doing now?' />
       <main className='mainContainer'>
+        <ul className='categoryContainer'>
+          {categorys.map((category) =>
+            category.selected ? (
+              <li
+                key={category.id}
+                className='category selected'
+                onClick={handleSelectedCategory}
+              >
+                {category.name}
+              </li>
+            ) : (
+              <li
+                key={category.id}
+                className='category'
+                onClick={handleSelectedCategory}
+              >
+                {category.name}
+              </li>
+            )
+          )}
+        </ul>
         <form onSubmit={(e) => e.preventDefault()}>
           <textarea
             className='writeBoard'
@@ -60,6 +111,20 @@ const Write = () => {
             onChange={handleWriteText}
           />
         </form>
+        <div className='photoContainer'>
+          {photos[3].file ? (
+            <IoMdPhotos className='photoIcon disabled' />
+          ) : (
+            <IoMdPhotos className='photoIcon' />
+          )}
+          {photos.map((photo) =>
+            photo.file ? (
+              <span key={photo.id} className='countPhotoBox fill'></span>
+            ) : (
+              <span key={photo.id} className='countPhotoBox'></span>
+            )
+          )}
+        </div>
         <div className='propContainer'>
           <div className='lengthContainer'>
             <NoticeCurcle noticeColor={setColor()} /> {textValue.length} / 140
@@ -110,8 +175,29 @@ const Container = styled.div`
     width: 100%;
     height: 100%;
 
+    .categoryContainer {
+      display: flex;
+      margin-bottom: 10px;
+
+      .category {
+        margin-right: 5px;
+        padding: 5px;
+        border: 1px solid ${({ theme }) => theme.mainColor};
+        border-radius: 5px;
+        font-size: 14px;
+        color: ${({ theme }) => theme.mainColor};
+        cursor: pointer;
+      }
+
+      .selected {
+        background-color: ${({ theme }) => theme.mainColor};
+        color: #fff;
+      }
+    }
+
     form {
       height: 40%;
+      margin-bottom: 10px;
 
       .writeBoard {
         width: 100%;
@@ -119,6 +205,38 @@ const Container = styled.div`
         padding: 20px;
         border-radius: 10px;
         border: 1px solid ${({ theme }) => theme.border};
+      }
+    }
+
+    .photoContainer {
+      display: flex;
+      align-items: center;
+      margin-bottom: 10px;
+
+      .photoIcon {
+        margin-right: 10px;
+        color: ${({ theme }) => theme.mainColor};
+        font-size: 1.5rem;
+        cursor: pointer;
+      }
+
+      .disabled {
+        color: ${({ theme }) => theme.text};
+        cursor: not-allowed;
+      }
+
+      .countPhotoBox {
+        display: inline-block;
+        width: 1rem;
+        height: 1rem;
+        margin-right: 5px;
+        border: 1px solid ${({ theme }) => theme.mainColor};
+        border-radius: 50%;
+      }
+
+      .fill {
+        border: none;
+        background-color: ${({ theme }) => theme.mainColor};
       }
     }
 
