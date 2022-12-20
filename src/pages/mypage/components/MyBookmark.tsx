e087a15b06ab9c6867fa6ff2a0d8f55d4338eb9b
Bong <2021bongwonny@gmail.com>
2022-12-21 00:50:39 +0900

    🚧Add : MyFeeds와 MyBookmark 컴포넌트에 통신 코드 추가 - MyFeeds와 MyBookmark 컴포넌트에 통신 코드 추가 - 필요 데이터 대한 UI만 추가

--- src/pages/mypage/components/MyBookmark.tsx
+++ src/pages/mypage/components/MyBookmark.tsx
@@ -1,3 +1,63 @@
-const MyFeeds = () => <h1>bookmarks</h1>;
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import { MainFeedStateType } from '../../../utils/interface';
+
+const MyFeeds = () => {
+  const [myBookmarks, setmyBookmarks] = useState<
+    MainFeedStateType[] | undefined
+  >();
+
+  useEffect(() => {
+    axios
+      .get('../data/feeds.json')
+      .then((res) => setmyBookmarks(res.data.feeds))
+      .catch((err) => console.log(err));
+
+    // axios
+    //   .get('http://localhost:8000/users/my', {
+    //     headers: { Authorization: localStorage.getItem('token') },
+    //   })
+    //   .then((res) => setmyBookmarks(res.data))
+    //   .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <Container>
+      <h4>북마크</h4>
+      {myBookmarks?.map((feed) => (
+        <div className='listBox' key={feed.id}>
+          <p>{feed.category}</p>
+          <p>{feed.content}</p>
+          <p>{feed.images ? '있음' : '없음'}</p>
+          <p>{feed.count_likes}</p>
+          <p>{feed.count_comments}</p>
+          <p>{feed.created_at}</p>
+        </div>
+      ))}
+    </Container>
+  );
+};
 
 export default MyFeeds;
+
+const Container = styled.div`
+  margin-bottom: 100px;
+  overflow: scroll;
+
+  h4 {
+    margin-bottom: 30px;
+    color: ${({ theme }) => theme.mainColor2};
+    font-size: 1.5rem;
+    text-align: center;
+  }
+
+  .listBox {
+    padding: 5px 0;
+    border-bottom: 1px solid ${({ theme }) => theme.border};
+
+    &:last-child {
+      border-bottom: none;
+    }
+  }
+`;
--- src/pages/mypage/components/MyFeeds.tsx
+++ src/pages/mypage/components/MyFeeds.tsx
@@ -1,3 +1,61 @@
-const MyFeeds = () => <h1>feeds</h1>;
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import { MainFeedStateType } from '../../../utils/interface';
+
+const MyFeeds = () => {
+  const [myFeeds, setMyFeeds] = useState<MainFeedStateType[] | undefined>();
+
+  useEffect(() => {
+    axios
+      .get('../data/feeds.json')
+      .then((res) => setMyFeeds(res.data.feeds))
+      .catch((err) => console.log(err));
+
+    // axios
+    //   .get('http://localhost:8000/users/my', {
+    //     headers: { Authorization: localStorage.getItem('token') },
+    //   })
+    //   .then((res) => setMyFeeds(res.data))
+    //   .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <Container>
+      <h4>내 게시글</h4>
+      {myFeeds?.map((feed) => (
+        <div className='listBox' key={feed.id}>
+          <p>{feed.category}</p>
+          <p>{feed.content}</p>
+          <p>{feed.images ? '있음' : '없음'}</p>
+          <p>{feed.count_likes}</p>
+          <p>{feed.count_comments}</p>
+          <p>{feed.created_at}</p>
+        </div>
+      ))}
+    </Container>
+  );
+};
 
 export default MyFeeds;
+
+const Container = styled.div`
+  margin-bottom: 100px;
+  overflow: scroll;
+
+  h4 {
+    margin-bottom: 30px;
+    color: ${({ theme }) => theme.mainColor2};
+    font-size: 1.5rem;
+    text-align: center;
+  }
+
+  .listBox {
+    padding: 5px 0;
+    border-bottom: 1px solid ${({ theme }) => theme.border};
+
+    &:last-child {
+      border-bottom: none;
+    }
+  }
+`;
